11aa5716997d842b5861fdebd39dfc15cbdb8ff3
Ivan <ipkozyrin@gmail.com>
2026-03-01 14:47:19 -0300

    add

---
+++ src/pages/fullscreen.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+});
+</script>
+
+<template>
+  <div class="h-screen w-screen bg-ui-bg text-ui-text overflow-hidden">
+    <MonitorContainer is-fullscreen />
+  </div>
+</template>
