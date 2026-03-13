7108574907aebf087538d42ef9dca97373ed60c6
Ivan <ipkozyrin@gmail.com>
2026-03-13 09:45:51 -0300

    add

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import { useProjectActions } from '~/composables/editor/useProjectActions';
 import ProjectsScreen from '~/components/startup/ProjectsScreen.vue';
 
 const { resetProjectState } = useProjectActions();
 const route = useRoute();
 const router = useRouter();
+const { isMobile } = useDevice();
 
 // Принудительно сбрасываем состояние проекта в Pinia при попадании на корень
 resetProjectState();
 
-onMounted(() => {
-  // Детекция мобильного устройства
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ||
-    window.innerWidth < 768;
-
-  // Если мобильное устройство и не выбран принудительный десктопный режим
-  if (isMobile && route.query.mode !== 'desktop') {
-    router.push('/m');
-    return;
-  }
-});
+// Если мобильное устройство и не выбран принудительный десктопный режим
+if (isMobile && route.query.mode !== 'desktop') {
+  router.replace('/m');
+}
 </script>
 
 <template>
