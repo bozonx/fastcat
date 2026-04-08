91cfc22fdabd22597079738a18df7c7ac216bcc3
Ivan <ipkozyrin@gmail.com>
2026-04-08 10:44:56 -0300

    test

--- test/unit/composables/timeline/useTimelineDropHandling.test.ts
+++ test/unit/composables/timeline/useTimelineDropHandling.test.ts
@@ -59,7 +59,7 @@ describe('useTimelineDropHandling', () => {
     mediaStore.mediaMetadata = {
       '_video/new.mp4': {
         source: { size: 1, lastModified: 1 },
-        duration: 1_500_000,
+        duration: 1.5,
         video: {
           width: 1920,
           height: 1080,
