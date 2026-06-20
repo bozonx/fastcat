3ae0391b382498fb151bc6a9e90246987fd40797
Ivan Kozyrin <ipkozyrin@gmail.com>
2026-06-19 22:36:53 -0300

    fix

--- src/components/timeline/MobileTimelineDrawer.vue
+++ src/components/timeline/MobileTimelineDrawer.vue
@@ -17,7 +17,7 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  toolbarSnapHeight: '94px',
+  toolbarSnapHeight: '106px',
   toolbarSnapWidth: '84px',
   withToolbarSnap: false,
   initialMode: 'toolbar',
